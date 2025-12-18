d035bce5afdd5bcc8ef1858c8f4aa5b8111455f2
Shakhrizat <253612@astanait.edu.kz>
2025-12-18 12:52:53 +0500

    1

---
+++ src/Clothingitem.java
@@ -0,0 +1,8 @@
+public class Clothingitem {
+    private int itemid;
+    private String name;
+    private int price;
+    private char size;
+    private boolean IsInStock;
+
+}
---
+++ src/Employee.java
@@ -0,0 +1,7 @@
+public class Employee {
+    private int itemid;
+    private String name;
+    private int price;
+    private char size;
+    private boolean isInStock;
+}
